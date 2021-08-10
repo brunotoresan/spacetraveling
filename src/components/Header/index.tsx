16c0e1d516dc37ee7a07ebae4ddab0a24b5721b7
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-10 08:12:15 -0300

    styles adjustments

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link'
 import style from './header.module.scss'
 
 export default function Header() {
   return (
-    <header className={style.headerContainer}>
-      <div className={style.headerContent}>
-        <img src="/logo.svg" alt="logo" />
-      </div>
-    </header>
+    <Link href='/' >
+      <header className={style.headerContainer}>
+        <div className={style.headerContent}>
+          <img src="/logo.svg" alt="logo" />
+        </div>
+      </header>
+    </Link>
   )
 }
